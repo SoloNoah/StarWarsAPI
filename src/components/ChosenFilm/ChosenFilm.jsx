271908396598f6a33fdfbcddaf5b023b641f0d17
SoloNoah <39374814+SoloNoah@users.noreply.github.com>
2021-08-11 16:47:58 +0300

    Updated fonts, need to apply in css, fixed widths

--- src/components/ChosenFilm/ChosenFilm.jsx
+++ src/components/ChosenFilm/ChosenFilm.jsx
@@ -4,17 +4,7 @@ import { connect } from 'react-redux';
 import ChosenFilmCard from './ChosenFilmCard';
 
 const ChosenFilm = ({ selectedMovie }) => {
-  return (
-    <>
-      {!selectedMovie ? (
-        <div className='chosenfilm__placeholder'>
-          <h1>Select a movie</h1>
-        </div>
-      ) : (
-        <ChosenFilmCard movie={selectedMovie} />
-      )}
-    </>
-  );
+  return <ChosenFilmCard movie={selectedMovie} />;
 };
 
 const mapStateToProps = (state) => ({
--- src/components/ChosenFilm/ChosenFilmCard.jsx
+++ src/components/ChosenFilm/ChosenFilmCard.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 
 const ChosenFilmCard = ({ movie }) => {
   return (
-    <div className='chosenfilm'>
-      <h1>{movie.title}</h1>
-      <p>{movie.opening_crawl}</p>
-      <button>Like</button>
-    </div>
+    <React.Fragment>
+      {!movie ? (
+        <div className='chosenfilm'>
+          <h1>Select a movie</h1>
+        </div>
+      ) : (
+        <div className='chosenfilm'>
+          <h1>{movie.title}</h1>
+          <p>{movie.opening_crawl}</p>
+          <button>Like</button>
+        </div>
+      )}
+    </React.Fragment>
   );
 };
 
