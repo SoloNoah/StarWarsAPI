6ce6501783a27ce1b5b7fb138e3b022bd566306e
SoloNoah <39374814+SoloNoah@users.noreply.github.com>
2021-08-13 11:30:19 +0300

    TOC & responsiveness done

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ListItem from './ListItem';
 
 const List = ({ movies }) => {
+  const [slide, setSlide] = useState(false);
+  const showSidebar = () => setSlide(!slide);
+
   return (
-    <ul className='toc_list'>
-      {movies.map((movie, index) => (
-        <ListItem movie={movie} key={index} />
-      ))}
-    </ul>
+    <React.Fragment>
+      <div onClick={showSidebar} className={slide ? 'burger cross' : 'burger'} id='burger'>
+        <div className='line-1'></div>
+        <div className='line-2'></div>
+        <div className='line-3'></div>
+      </div>
+      <ul className={slide ? 'toc_list active' : 'toc_list'}>
+        {movies.map((movie, index) => (
+          <ListItem movie={movie} key={index} />
+        ))}
+      </ul>
+    </React.Fragment>
   );
 };
 
